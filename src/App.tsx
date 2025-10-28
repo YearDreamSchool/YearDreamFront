2fc02aab12de60ac77ff41b60ab51d4c70c18f54
do_ong22 <soonduck3729@gmail.com>
2025-10-28 13:23:26 +0900

    Fix: App 롤백

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Page from "page"
-import OAuthRedirectHandler from "./components/page/OAuthRedirectHandler"
+import Page from "page";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Page />} />
-        <Route path="/oauth/redirect" element={<OAuthRedirectHandler />} />
-        <Route path="/login/oauth2/code/google" element={<OAuthRedirectHandler />} />
-      </Routes>
-    </Router>
-  )
+  return <Page />;
 }
